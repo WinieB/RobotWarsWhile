67fc7ff7df4c8d157dc553080ec867b0cf2e482d
Brolle <winienicklas.brolle@btc-ag.com>
2024-09-23 13:05:44 +0200

    welcome Screen hinzugefügt, Begrüßung, Bug fixes

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -7,13 +7,32 @@ public class Main {
     private static final int ROWS = 10;
     private static final int COLS = 15;
 
-    private static int roboRows = 8;
-    private static int roboCols = 13;
+    private static int roboRows = 0;
+    private static int roboCols = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // welcomeScreen();
+        System.out.println(
+                        "================================\n" +
+                        "         Willkommen bei         \n" +
+                        "            ROBOWARS            \n" +
+                        "================================\n" +
+                        "  ,__,_,\n" +
+                        "  [_|_/ \n" +
+                        "   //\n" +
+                        " _//    __\n" +
+                        "(_|)   |@@|\n" +
+                        " \\ \\__ \\--/ __\n" +
+                        "  \\o__|----|  |   __\n" +
+                        "      \\ }{ /\\ )_ / _\\\n" +
+                        "      /\\__/\\ \\__O (__\n" +
+                        "     (--/\\--)    \\__/\n" +
+                        "     _)(  )(_\n" +
+                        "    `---''---`\n" +
+                        "================================\n" +
+                        "      Bereit für den Kampf?     \n" +
+                        "================================\n");
 
         System.out.println("Was ist dein Name Krieger?");
         String playerName = scanner.nextLine();
@@ -21,7 +40,7 @@ public class Main {
         System.out.println("\nWillkommen " + playerName + ", wie soll deiner Kampfmaschiene heißen?");
         String roboName = scanner.nextLine();
 
-        System.out.println("\nDann auf in den Kampf!");
+        System.out.println("\nNa los " + playerName + ", krall dir " + roboName + " und auf in den Kampf!");
 
        // field();
 
