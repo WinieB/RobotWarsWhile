1df6337971a1cd593b73188133938e2ad5ae2de8
Brolle <winienicklas.brolle@btc-ag.com>
2024-09-24 14:03:21 +0200

    Bugfixes, Kommentare, leichte Überarbeitung

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -3,18 +3,17 @@ package com.btcag.bootcamp;
 import java.util.Scanner;
 
 public class Main {
-
-    private static final int ROWS = 10;
-    private static final int COLS = 15;
-
-    private static int roboRows = 0;
-    private static int roboCols = 0;
-
     public static void main(String[] args) {
+        int ROWS = 10; // Spielfeldgröße
+        int COLS = 15;
+
+        int roboRows = 0; // Roboterkoordinaten
+        int roboCols = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(
-                "================================\n" +
+        System.out.println( // welcome Screen
+                        "================================\n" +
                         "         Willkommen bei         \n" +
                         "            ROBOWARS            \n" +
                         "================================\n" +
@@ -34,40 +33,42 @@ public class Main {
                         "      Bereit für den Kampf?     \n" +
                         "================================\n");
 
-        System.out.println("Was ist dein Name Krieger?");
+        System.out.println("Was ist dein Name Krieger?"); // Spielername
         String playerName = scanner.nextLine();
 
-        System.out.println("\nWillkommen " + playerName + ", wie soll deiner Kampfmaschiene heißen?");
+        System.out.println("\nWillkommen " + playerName + ", wie soll deiner Kampfmaschiene heißen?"); // Robotername
         String roboName = scanner.nextLine();
 
-        System.out.println("\nNa los " + playerName + ", krall dir " + roboName + " und auf in den Kampf!");
+        System.out.println("\nNa los " + playerName + ", krall dir " + roboName + " und auf in den Kampf!"); // Begrüßung
 
 
+
+        // Spielfeld
         int i = 0;
-        while (i < ROWS) {
+        while (i < ROWS) { // Hochzählung ROWS
             int j = 0;
 
-            while (j < COLS) {
+            while (j < COLS) { // Hochzählung COLS
                 int tempRoboRow = roboRows;
                 int tempRoboCol = roboCols;
 
                 System.out.print("[");
-                String inhalt = " ";
+                String inhalt = "  ";
 
                 while (i == tempRoboRow) {
-                    while (j == tempRoboCol) {
-                        inhalt = "a";
-                        tempRoboCol++;
+                    while (j == tempRoboCol) { // Wenn Roboterkoordinaten x & y, wird der leere String überschrieben
+                        inhalt = "\uD83E\uDD16"; // Robotersymbol
+                        tempRoboCol++; // tempRoboCol++ damit aus der schleife rausgegangen wird
                     }
-                    tempRoboRow++;
+                    tempRoboRow++; // tempRoboRow++ damit aus der schleife rausgegangen wird
                 }
 
                 System.out.print(inhalt + "]");
-                j++;
+                j++; // j++ zum Hochzählen
 
             }
-            System.out.println();
-            i++;
+            System.out.println(); // Übersprung in die nächste Zeile
+            i++; // i++ zum Hochählen
 
         }
     }
